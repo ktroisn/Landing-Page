104d2617fd058ccf9bf9da3d6e376378d704e12b
ktroisn <brouillonquentin@gmail.com>
2023-05-15 14:58:52 +0200

    design adjustment and functional burger menu
    there are still some points to correct on the design level, the burger menu works, there is an animation when opening and closes when clicking on a link

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 function toggleMenu () {
     const main_navigation = document.querySelector('.main-navigation');
     const burger = document.querySelector('.burger');
+    const menu_items = main_navigation.querySelectorAll('a');
     burger.addEventListener('click', () => {
         main_navigation.classList.toggle('open-burger');
-    })
+    });
+
+    menu_items.forEach(item => {
+        item.addEventListener('click', () => {
+            main_navigation.classList.remove('open-burger');
+        });
+    });
 }
 toggleMenu();
